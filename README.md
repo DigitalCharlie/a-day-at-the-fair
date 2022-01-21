89aa81a66b201863736899ca2d302a18dd6ebff1
Digital Charlie <furman.jr@gmail.com>
2022-01-20 21:02:46 -0800

    Start of game is there, some images, and learning how to make enter work like clicking submit

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A text adventure set in the Forgotten Realms.
 
 Story adapted from ["The Pudding Faire,"](https://www.dmsguild.com/product/249757/Pudding-Faire) a D&D 5e module by Will Doyle.
 
-Art is from Ross McConnell at [2 Minute Tabletop](https://2minutetabletop.com/)
+Maps are from Ross McConnell at [2 Minute Tabletop](https://2minutetabletop.com/)
--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log('hello world')
+let adventurer = ""
+
+const intro = document.getElementById('intro')
+const nameAdvBtn = document.getElementById('intro-button')
+const nameInput = document.getElementById('adventurer-name')
+
+const currentText = document.getElementById('current-text')
+const options = document.getElementById('current-options')
+
+
+// NAME YOURSELF AND THEN START THE GAME
+
+const nameAdventurer = () => {
+    if (nameInput.value) { // If the name input is not blank
+        adventurer = nameInput.value; // name them the input value
+        nameInput.blur() // blur the field so they don't hit enter a bunch and change it
+    } else {
+        adventurer = "Traveler" // default if it is blank — this will get used as easter egg dialogue later
+    }
+    console.log('Adventurer is now named ' + adventurer)
+    intro.hidden = true
+    currentText.hidden = false
+    options.style.display = "block"
+}
+
+
+
+
+
+
+// EVENT LISTENERS
+
+nameAdvBtn.addEventListener('click', nameAdventurer)
+nameInput.addEventListener('keypress', (e) => {
+    if (e.key == "Enter") {
+        nameAdvBtn.click();
+    }
+})
