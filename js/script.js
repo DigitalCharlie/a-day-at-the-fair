82dd0bd68b137c9ac245768e04e9c0e0a7b7aeff
Digital Charlie <furman.jr@gmail.com>
2022-01-25 19:07:30 -0800

    Scroll to bottom of section now working

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ const bg = document.getElementById('bg')
 let dayCount = 0
 let goodDeeds = 0
 let badDeeds = 0
+let timeOfDay = 7
 
 // FADE IN THE SCREEN IN STAGES ... AND MAKE BUTTON COLORS RANDOM
 
@@ -53,7 +54,6 @@ nameInput.addEventListener('keypress', (e) => {
     }
 })
 
-
 // HOW TO ADD TEXT TO TEXT ADVENTURE SECTION
 
 const clearButtons = () => { // clears the buttons by emptying the innerHTML of optionsBox
@@ -64,6 +64,10 @@ const clearText = () => { // clears the story text by emptying the innerHTML of 
     currentText.innerHTML = ''
 }
 
+const removeSelectedButton = (button) => {
+    button.remove()
+}
+
 const addToCurrentText = (text) => {
     let newText = document.createElement('li') // create a new list item
     newText.textContent = text // make the text of the new list item equal to whatever the input text is
@@ -71,6 +75,7 @@ const addToCurrentText = (text) => {
     newHistoryText.textContent = text
     currentText.appendChild(newText) // adds it to the UL that holds text
     historyContents.lastElementChild.appendChild(newHistoryText) // adds it to the history as well
+    currentText.scrollTop = currentText.scrollHeight
 }
 
 const continueButton = (eventName) => {
@@ -82,8 +87,39 @@ const continueButton = (eventName) => {
     buttonColorIsRandom()
 }
 
-const removeSelectedButton = (button) => {
-    button.remove()
+const advanceTime = (timeTaken) => {
+    timeOfDay += timeTaken
+}
+
+const changeDailyConditions = (condition) => {
+    for (i = 0; i < condition.length; i++) {
+        dailyConditions[condition[i]] = true
+    }
+}
+
+const changePermConditions = (condition) => {
+    for (i = 0; i < condition.length; i++) {
+        permConditions[condition[i]] = true
+    }
+}
+
+const newEvent = (eventName) => {
+    clearButtons() // clear all buttons
+    clearText() // clear all text
+    if (eventName === beginNewDay) {
+        dayCount++ // advance day counter
+        timeOfDay = 7 // set time to 7am
+        goodDeeds = 0
+        badDeeds = 0
+        resetDailyConditions()
+        newHistoryDay() // add a new day header in the history bar
+    }
+    addToCurrentText(eventName.intro) // put the intro text in the box for the event
+    createOptionButtons(eventName) // create the option buttons for the event
+    if(eventName.eventBg) {
+        newBg(eventName.eventBg)
+    }
+    buttonColorIsRandom()
 }
 
 const createOptionButtons = (eventName) => {
@@ -91,6 +127,7 @@ const createOptionButtons = (eventName) => {
         const newBtn = document.createElement('button') // create a button
         newBtn.textContent = eventName.options[i].button // make the name of the button
         newBtn.addEventListener('click', ()=>{addToCurrentText(eventName.options[i].text)}) // makes it so clicking the button adds text to the li
+        newBtn.addEventListener('click', ()=>{advanceTime(eventName.options[i].duration)}) // increases current time
         if (eventOptions[eventName.options[i].continue]) { // checks if there is a continue location — if yes, display it.
             newBtn.addEventListener('click', ()=>{continueButton(eventOptions[eventName.options[i].continue])})  // makes it so clicking the button adds an appropriate continue button
         } else {
@@ -98,6 +135,12 @@ const createOptionButtons = (eventName) => {
         }
         if (eventName.options[i].bg) {
             newBtn.addEventListener('click', ()=>{newBg(eventName.options[i].bg)}) 
+        }
+        if (eventName.options[i].dailyConChanges) {
+            newBtn.addEventListener('click', ()=>{changeDailyConditions(eventName.options[i].dailyConChanges)}) 
+        }
+        if (eventName.options[i].permConChanges) {
+            newBtn.addEventListener('click', ()=>{changePermConditions(eventName.options[i].permConChanges)}) 
         }
         optionsBox.appendChild(newBtn) // adds a new button
     }
@@ -112,6 +155,7 @@ const createOptionButtons = (eventName) => {
                 newBtn.classList.add("hidden-option") // give it a class that gives it transition styles
                 newBtn.textContent = eventName.hiddenOptions[i].button
                 newBtn.addEventListener('click', ()=>{addToCurrentText(eventName.hiddenOptions[i].text)})
+                newBtn.addEventListener('click', ()=>{advanceTime(eventName.hiddenOptions[i].duration)}) // increases current time
                 if (eventOptions[eventName.hiddenOptions[i].continue]) {
                     newBtn.addEventListener('click', ()=>{continueButton(eventOptions[eventName.hiddenOptions[i].continue])})  // makes it so clicking the button adds an appropriate continue button
                 } else {
@@ -119,6 +163,12 @@ const createOptionButtons = (eventName) => {
                 }
                 if (eventName.hiddenOptions[i].bg) {
                     newBtn.addEventListener('click', ()=>{newBg(eventName.hiddenOptions[i].bg)}) 
+                }
+                if (eventName.hiddenOptions[i].dailyConChanges) {
+                    newBtn.addEventListener('click', ()=>{changeDailyConditions(eventName.hiddenOptions[i].dailyConChanges)}) 
+                }
+                if (eventName.hiddenOptions[i].permConChanges) {
+                    newBtn.addEventListener('click', ()=>{changePermConditions(eventName.hiddenOptions[i].permConChanges)}) 
                 }
                 optionsBox.appendChild(newBtn)
                 if (eventName.hiddenOptions[i].alreadyDisplayed === false) {
@@ -133,26 +183,9 @@ const createOptionButtons = (eventName) => {
     }
 }
 
-
-const newEvent = (eventName) => {
-    clearButtons() // clear all buttons
-    clearText() // clear all text
-    if (eventName === beginNewDay) {
-        dayCount++
-        newHistoryDay()
-    }
-    addToCurrentText(eventName.intro) // put the intro text in the box for the event
-    createOptionButtons(eventName) // create the option buttons for the event
-    if(eventName.eventBg) {
-        newBg(eventName.eventBg)
-    }
-    buttonColorIsRandom()
-}
-
 // BACKGROUND TOGGLES
 
 const newBg = (backgroundImage) => {
-    console.log(backgroundImage)
     const oldBg = document.querySelector("#bg div:nth-child(1)")
     const newBg = document.createElement('div')
     newBg.classList.add('bg-image')
@@ -167,7 +200,7 @@ const toggleBg = () => {
     const bottomBg = document.querySelector("#bg div:nth-child(1)")
     topBg.style.opacity = 0
     bottomBg.style.opacity = 1
-    setTimeout(() => {topBg.remove()}, 1000);
+    setTimeout(() => {topBg.remove()}, 500);
 }
   
 
@@ -238,13 +271,13 @@ const beginNewDay = {
     eventBg: 'img/town-day.jpg',
     intro: `The crow of a rooster awakens you from slumber, followed by the sound of a brass band cranking up. "Roll up! Roll up for the Pudding Faire!” cries a voice from the street below. Peering out the inn’s window, you see a crowd of happy halflings and gnomes bustling toward a fairground on the village green. It is seven o’clock in he morning on the day of the annual Honeypuddle Pudding Faire!`,
     options: [
-        {button: "Go back to bed", text: "Your head swirls and you decide that this is just too much right now. Perhaps later in the day, after a bit more sleep and a hearty meal from downstairs, the whole situation will feel a bit more manageable."}, 
-        {button: "Dash off to the fair", bg: 'img/town-day.jpg', text: "You hurry to put on your boots and rush out of the inn, not wanting to waste a single moment of the day.", continue: 1},
-        {button: "Observe for a few minutes", bg: 'img/the-faire.png', text: "You gaze out the window and take in the sight of the fair. You see gnomes performing a maypole dance, a halfling tightrope walking above a crowd of spectators, a few dancers juggling knives, and a dozen games of skill and chance."},
-        {button: "Try to remember your dreams", bg: 'img/town-day.jpg', text: "Before getting out of bed, you close your eyes and try to focus on your dreams from the night before — in your mind's eye, you see a white mole with claws of steel laughing in front of  woman turned to stone. It's a bizarre and haunting image.", continue: 1},
+        {button: "Go back to bed", duration: 2, text: "Your head swirls and you decide that this is just too much right now. Perhaps later in the day, after a bit more sleep and a hearty meal from downstairs, the whole situation will feel a bit more manageable."}, 
+        {button: "Dash off to the fair", duration: 0, continue: 1, bg: 'img/town-day.jpg', text: "You hurry to put on your boots and rush out of the inn, not wanting to waste a single moment of the day."},
+        {button: "Observe for a few minutes", dailyConChanges: ['Avoided puddle','Played game'], duration: .5, bg: 'img/the-faire.png', text: "You gaze out the window and take in the sight of the fair. You see gnomes performing a maypole dance, a halfling tightrope walking above a crowd of spectators, a few dancers juggling knives, and a dozen games of skill and chance."},
+        {button: "Try to remember your dreams", duration: .5, continue: 1, bg: 'img/town-day.jpg', text: "Before getting out of bed, you close your eyes and try to focus on your dreams from the night before — in your mind's eye, you see a white mole with claws of steel laughing in front of  woman turned to stone. It's a bizarre and haunting image."},
     ],
     hiddenOptions: [
-        {condition: dayCount === 2, alreadyDisplayed: false, button: "Explore that sense of déjà vu ", bg: 'img/town-day.jpg', text: "Pulling yourself into consciousness, you listen to the sounds outside your window for a moment and swear that what you're hearing is almost identical to what you heard yesterday.", continue: 1}    
+        {condition: dayCount === 2, alreadyDisplayed: false, duration: .5, button: "Explore that sense of déjà vu ", continue: 1, bg: 'img/town-day.jpg', text: "Pulling yourself into consciousness, you listen to the sounds outside your window for a moment and swear that what you're hearing is almost identical to what you heard yesterday."}    
     ]
 }
 
@@ -252,14 +285,16 @@ const outsideTheInn = {
     eventBg: 'img/the-faire.png',
     intro: "This is a test of the event system placed inside of objects. Hopefully it is working properly",
     options: [
-        {button: "Option 1", text: "You chose option 1", continue: 0}, 
-        {button: "Option 2", text: "You chose option 2", continue: 0},
+        {button: "Option 1", duration: 1, text: "You chose option 1", continue: 0}, 
+        {button: "Option 2", duration: 1, text: "You chose option 2", continue: 0},
     ],
     hiddenOptions: [
-        {condition: dayCount >= 2, alreadyDisplayed: false, button: "Option 3", text: "You chose option 3 — which should only happen after day 1", continue: 0},
-        {condition: dayCount >= 2, alreadyDisplayed: false, button: "Option 4", text: "You chose option 3 — which should only happen after day 1", continue: 0}
+        {condition: dayCount >= 2, duration: 2, alreadyDisplayed: false, button: "Option 3", text: "You chose option 3 — which should only happen after day 1", continue: 0},
+        {condition: dayCount >= 2, duration: 2, alreadyDisplayed: false, button: "Option 4", text: "You chose option 3 — which should only happen after day 1", continue: 0}
     ]
 }
+
+// I HATE HOW BOTH OF THESE WORK AND THERE SHOULD BE SOMETHING BETTER BUT I DON'T KNOW WHAT
 
 const eventOptions = { // I'm setting this up to be called with bracket notation because i want to be able to see the pairs more easily — giving them all names feels weird since they're placeholders to call an already named function?
     0: beginNewDay,
@@ -271,6 +306,26 @@ const resetHiddenConditions = () => {
     outsideTheInn.hiddenOptions[0].condition = dayCount >= 2
     outsideTheInn.hiddenOptions[1].condition = dayCount >= 2
 }
+
+// HIDDEN CONDITIONS THAT ARE USED BY EVENTS TO TRIGGER RESULTS
+// and the function to reset the daily ones
+// all daily conditions are under 100. Permanent ones are over 100.
+
+const dailyConditions = {
+    'Avoided puddle': true, // prevented stepping in the puddle
+    'Played game': true, // played a game
+}
+
+const permConditions = {
+
+}
+
+const resetDailyConditions = () => {
+    for (condition in dailyConditions) {
+        dailyConditions[condition] = false;
+    }
+}
+
 
 // STUFF AT THE END
 
@@ -298,15 +353,10 @@ Close up of characters --> when talking to people?
 
 
 Possible pitfalls:
-- might want to stage the hidden conditions and run them through a loop rather than assume they're all there.
 - will have some slightly redundant events that will serve as continuations — need to do something for after they select a few options how buttons may change.
     - ex: can they pick all the options in the room before having to continue? Or after a couple does it force forwards (if possible)?
 - assign time increments to buttons
 - make the hidden options distinct — so hidden option 1, hidden option 2, and then add "first time displayed" to determine whether it gets the fade in or just happen immediately.
 
-
-if I have multiple hidden conditions, should I instead make the statement be a loop for those hidden conditions and store them inside of their own object in the event object? So then there can be multiple?
-    - similar to options.
-    - if statement is for the event as a whole, if it exists.
 
 */
